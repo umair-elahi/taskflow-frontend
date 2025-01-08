399215683a67048f479158308d6f6357d6fd3610
umair elahi <a.wahab.katib@gmail.com>
2025-01-08 10:02:04 +0500

    resolve duplicate dashboard component

--- src/app/admin/configuration/dashboard/dashboard.module.ts
+++ src/app/admin/configuration/dashboard/dashboard.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { DashboardComponent } from './dashboard.component';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { DashboardComponent } from './dashboard.component';
 
-@NgModule({
-  declarations: [DashboardComponent],
-  imports: [CommonModule],
-  exports: [DashboardComponent]
-})
-export class DashboardModule { }
+// @NgModule({
+//   declarations: [DashboardComponent],
+//   imports: [CommonModule],
+//   exports: [DashboardComponent]
+// })
+// export class DashboardModule { }
