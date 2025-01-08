ff06e09b01e8b821498525ea18db7707f73c514c
umair elahi <a.wahab.katib@gmail.com>
2025-01-08 12:14:18 +0500

    base url updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBase: 'https://workable.aetasaal.com/api/',
-  apiImage: 'https://workable.aetasaal.com/api/images/',
+  apiBase: 'https://aetasaal.duckdns.org/api/',
+  apiImage: 'https://aetasaal.duckdns.org/api/images/',
   downloadimage: 'http://34.199.172.154/uploads/',
   appName: 'Taskflow 2.0',
   googleAPIKey: 'AIzaSyAC_Sr8Spc8-D5wjzMGlh0EaPHsLhEtxE0',
