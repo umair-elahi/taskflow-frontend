09e6fa4520566adf035e0d451d6ae160fc5bf0a3
umair elahi <a.wahab.katib@gmail.com>
2025-01-11 16:37:01 +0500

    successful build for final deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiBase: 'http://localhost:3000/api/',
-  apiImage: 'http://localhost:3000/images/',
+  apiBase: 'https://aetasaal.duckdns.org:3000/api/',
+  apiImage: 'https://aetasaal.duckdns.org:3000/images/',
   downloadimage: 'http://34.199.172.154/uploads/',
   appName: 'Taskflow 2.0',
   googleAPIKey: 'AIzaSyAC_Sr8Spc8-D5wjzMGlh0EaPHsLhEtxE0',
