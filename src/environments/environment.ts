cf41fc12f5a074909d6b086827e923afc929858c
umair elahi <a.wahab.katib@gmail.com>
2025-01-11 11:54:24 +0500

    some changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   apiBase: 'http://localhost:3000/api/',
-  apiImage: 'https://localhost:3000/images/',
+  apiImage: 'http://localhost:3000/images/',
   downloadimage: 'http://34.199.172.154/uploads/',
   appName: 'Taskflow 2.0',
   googleAPIKey: 'AIzaSyAC_Sr8Spc8-D5wjzMGlh0EaPHsLhEtxE0',
