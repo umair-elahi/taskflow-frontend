d4fddcfd27b40653b7c9c1f3b533e15ac401f697
umair elahi <a.wahab.katib@gmail.com>
2025-01-21 12:19:59 +0500

    successfully resolved file related issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiBase: 'https://aetasaal.duckdns.org:3000/api/',
   apiImage: 'https://aetasaal.duckdns.org:3000/images/',
-  downloadimage: 'http://34.199.172.154/uploads/',
+  downloadimage: 'https://aetasaal.duckdns.org:3000/upload/',
   appName: 'Taskflow 2.0',
   googleAPIKey: 'AIzaSyAC_Sr8Spc8-D5wjzMGlh0EaPHsLhEtxE0',
   appVersion: 1.1,
