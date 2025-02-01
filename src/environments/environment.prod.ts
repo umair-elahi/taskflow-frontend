4e6bd04709d415dc779bae06ee20e86b6bcf12e8
umair elahi <a.wahab.katib@gmail.com>
2025-02-01 16:21:51 +0500

    changing port number

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiBase: 'https://aetasaal.duckdns.org:3000/api/',
-  apiImage: 'https://aetasaal.duckdns.org:3000/images/',
-  downloadimage: 'https://aetasaal.duckdns.org:3000/upload/',
+  apiBase: 'https://aetasaal.duckdns.org/api/',
+  apiImage: 'https://aetasaal.duckdns.org/images/',
+  downloadimage: 'https://aetasaal.duckdns.org/upload/',
   appName: 'Taskflow 2.0',
   googleAPIKey: 'AIzaSyAC_Sr8Spc8-D5wjzMGlh0EaPHsLhEtxE0',
   appVersion: 1.1,
