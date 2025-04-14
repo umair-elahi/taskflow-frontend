678099b5fffcc226fe59553a3f3baab020c173c5
umair elahi <a.wahab.katib@gmail.com>
2025-04-14 13:26:13 +0500

    domain changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiBase: 'https://aetasaal.duckdns.org/api/',
-  apiImage: 'https://aetasaal.duckdns.org/images/',
-  downloadimage: 'https://aetasaal.duckdns.org/upload/',
+  apiBase: 'https://taskskate.com/api/',
+  apiImage: 'https://taskskate.com/images/',
+  downloadimage: 'https://taskskate.com/upload/',
   appName: 'Taskflow 2.0',
   googleAPIKey: 'AIzaSyAC_Sr8Spc8-D5wjzMGlh0EaPHsLhEtxE0',
   appVersion: 1.1,
